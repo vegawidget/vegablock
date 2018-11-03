3daaca0455b0891a0e6002da6932ccb66f78dcd2
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-11-03 14:47:29 -0500

    adds functions

---
+++ R/utils-vegawidget.R
@@ -0,0 +1,38 @@
+#### vegaspec functions ####
+
+#' Coerce to vegaspec
+#'
+#' See \code{vegawidget::\link[vegawidget]{as_vegaspec}} for details.
+#'
+#' @name as_vegaspec
+#' @importFrom vegawidget as_vegaspec
+#' @export
+#'
+NULL
+
+#### vegawidget functions ####
+
+#' Vega embed options
+#'
+#' See \code{vegawidget::\link[vegawidget]{vega_embed}} for details.
+#'
+#' @name vega_embed
+#' @rdname vega_embed
+#' @importFrom vegawidget vega_embed
+#' @export
+#'
+NULL
+
+#### utility functions ####
+
+#' Determine Vega JavaScript versions
+#'
+#' See \code{vegawidget::\link[vegawidget]{vega_version}} for details.
+#'
+#' @name vega_version
+#' @rdname vega_version
+#' @importFrom vegawidget vega_version
+#' @export
+#'
+NULL
+
