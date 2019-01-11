1b1b3220a3dd492d193b224b63ee849851854ed5
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-01-11 11:56:25 -0600

    fixes bug in checking for node

--- R/utils-block.R
+++ R/utils-block.R
@@ -56,7 +56,7 @@ vw_block_build_directory <-
     # image
     if (use_thumbnail || use_preview) {
 
-      has_node <- length(Sys.which("node")) > 0L
+      has_node <- nchar(Sys.which("node")) > 0L
       has_magick <- requireNamespace("magick", quietly = TRUE)
 
       if (!has_magick) {
